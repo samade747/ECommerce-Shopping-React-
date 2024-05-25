8b6ee9f9e260f49a2c130315c0d36900522286e5
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-25 07:58:16 +0500

    758am 25 5 2024

---
+++ shopping/src/components/CategoryItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { styled } from 'styled-components'
+import { categories } from '../data'
+
+const Container = styled.div`
+
+
+`
+
+const CategoryItem = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CategoryItem
--- shopping/src/components/Slider.jsx
+++ shopping/src/components/Slider.jsx
@@ -19,6 +19,7 @@ const Container = styled.div`
 const Wrapper = styled.div`  
     height: 100%;
     display: flex; 
+    transition: all 1.5s ease;
     transform: translateX(${(props) => props.slideIndex * -100}vw);
 `
 
