3af60bb6fa95220ad8103ee73f5507a67a6c91dc
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-28 21:08:19 +0500

    z

--- shopping/src/components/Footer.jsx
+++ shopping/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    display: flex;
+    
     display: flex;
     flex-direction: column;
     
@@ -10,8 +10,29 @@ const Container = styled.div`
 const Left = styled.div`
     flex: 1;
     padding: 20px;
+    flex-direction: column;
     
 `
+
+const Logo = styled.h1`
+
+`    
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+
+const SocialContainer = styled.div`
+
+`
+
+const SocialIcon = styled.div`
+
+`
+
+
+
+
 const Center = styled.div`
     flex: 1;
     padding: 20px;
@@ -26,15 +47,39 @@ const Right = styled.div`
 const Footer = () => {
   return (
     <Container>
-        <Left></Left>
+        <Left>
+            <Logo>usa shopping</Logo>
+            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Desc>
+
+            <SocialContainer>
+                <SocialIcon>
+                    <FacebookIcon />                    
+                </SocialIcon>
+
+                <SocialIcon>
+                    <Instagram />                    
+                </SocialIcon>
+
+                <SocialIcon>
+                    <Twitter />                    
+                </SocialIcon>
+               
+            </SocialContainer>
+
+
+        </Left>
+
+        <Center>
+
+
+        </Center>
 
 
 
-        <Center></Center>
+        <Right>
 
 
-
-        <Right></Right>
+        </Right>
 
 
 
