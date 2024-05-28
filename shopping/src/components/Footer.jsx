8d08f1895b0ffa1fd863141ab9c490bfd5fe2bf6
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-29 00:46:12 +0500

    z

--- shopping/src/components/Footer.jsx
+++ shopping/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from '@mui/icons-material'
+
 
 const Container = styled.div`
-    
     display: flex;
     flex-direction: column;
     
@@ -20,16 +21,41 @@ const Logo = styled.h1`
 
 const Desc = styled.p`
     margin: 20px 0px;
+
 `
 
 const SocialContainer = styled.div`
+    display: flex;  
 
 `
 
 const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background-color: #${props => props.color};
 
 `
+const Title = styled.h3`
+    margin-bottom: 30px;
+`   
 
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+    
+`   
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
 
 
 
@@ -43,24 +69,59 @@ const Right = styled.div`
 
 `  
 
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Payment = styled.img`
+    width: 50%;
+`
+const PaymentContainer = styled.div`
+    display: flex;
+    align-items: center;
+`
+const PaymentIcon = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`
+
+const PaymentIconContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`
+
+
 
 const Footer = () => {
   return (
     <Container>
         <Left>
             <Logo>usa shopping</Logo>
-            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Desc>
+            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit
+                lorem ipsum dolor sit amet consectetur adipisicing elit
+                lorem ipsum dolor sit amet consectetur adipisicing elit
+           </Desc>
 
             <SocialContainer>
-                <SocialIcon>
-                    <FacebookIcon />                    
+                <SocialIcon color="3B5999">
+                    <Facebook />                    
                 </SocialIcon>
 
-                <SocialIcon>
+                <SocialIcon color="3B5999">
                     <Instagram />                    
                 </SocialIcon>
 
-                <SocialIcon>
+                <SocialIcon color="3B5999">
                     <Twitter />                    
                 </SocialIcon>
                
@@ -70,6 +131,24 @@ const Footer = () => {
         </Left>
 
         <Center>
+            <Title>Useful Links</Title>
+
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+
+            </List>
+
+
+
 
 
         </Center>
@@ -77,6 +156,23 @@ const Footer = () => {
 
 
         <Right>
+            <Title>
+                Contact                
+            </Title>
+
+            <ContactItem>
+                <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
+            </ContactItem>
+
+
+            <ContactItem>   
+                <Phone style={{marginRight:"10px"}}/> +1 234 56 78
+            </ContactItem>
+
+
+            <ContactItem>
+                <MailOutline style={{marginRight:"10px"}}/> 0k7pH@example.com
+            </ContactItem>
 
 
         </Right>
--- shopping/src/data.jsx
+++ shopping/src/data.jsx
@@ -2,21 +2,21 @@ export const SliderItems = [
     {
       id: 1,
         // adding image of girls with fashion
-      img: "https://via.placeholder.com/800x400",
+      img: "https://images.pexels.com/photos/1642228/pexels-photo-1642228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
     },
     {
       id: 2,
-      img: "https://via.placeholder.com/800x400",
+      img: "https://images.pexels.com/photos/2752045/pexels-photo-2752045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://via.placeholder.com/800x400",
+      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
@@ -28,27 +28,27 @@ export const SliderItems = [
   export const popularProducts = [
     {
       id: 1,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 2,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/994234/pexels-photo-994234.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 3,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 4,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/994234/pexels-photo-994234.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 5,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/994234/pexels-photo-994234.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 6,
-      img: "https://via.placeholder.com/200",
+      img: "https://images.pexels.com/photos/994234/pexels-photo-994234.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 7,
