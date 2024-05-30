39c9a58ad2b453d1a528740922942088d8575094
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-31 00:33:58 +0500

    1233am 31 5 2024

--- shopping/src/components/Footer.jsx
+++ shopping/src/components/Footer.jsx
@@ -1,228 +1,186 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from '@mui/icons-material'
+import React from 'react';
+import styled from 'styled-components';
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from '@mui/icons-material';
 
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    
-`   
+const ContentWrapper = styled.div`
+  flex: 1;
+`;
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fcf5f5;
+`;
+
 const Left = styled.div`
-    flex: 1;
-    padding: 20px;
-    flex-direction: column;
-    
-`
+  flex: 1;
+  padding: 20px;
+  flex-direction: column;
+`;
 
-const Logo = styled.h1`
-
-`    
+const Logo = styled.h1``;
 
 const Desc = styled.p`
-    margin: 20px 0px;
-
-`
+  margin: 20px 0px;
+`;
 
 const SocialContainer = styled.div`
-    display: flex;  
-
-`
+  display: flex;
+`;
 
 const SocialIcon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    background-color: #${props => props.color};
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color: #${(props) => props.color};
+`;
 
-`
 const Title = styled.h3`
-    margin-bottom: 30px;
-`   
+  margin-bottom: 30px;
+`;
 
 const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-    
-`   
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const ListItem = styled.li`
-    width: 50%;
-    margin-bottom: 10px;
-`
-
-
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 const Center = styled.div`
-    flex: 1;
-    padding: 20px;
-`
-const Right = styled.div`
-    flex: 1;
-    padding: 20px;
+  flex: 1;
+  padding: 20px;
+`;
 
-`  
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
 const ContactItem = styled.div`
-    margin-bottom: 20px;
-    display: flex;
-    align-items: center;
-`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
 
 const Payment = styled.img`
-    width: 50%;
-`
+  width: 50%;
+`;
+
 const PaymentContainer = styled.div`
-    display: flex;
-    align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
+
 const PaymentIcon = styled.div`
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    background-color: #${props => props.color};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 20px;
-`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const PaymentIconContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 20px;
-`
-
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
 const Footer = () => {
   return (
-    <Container>
+    <PageWrapper>
+      <ContentWrapper>
+        {/* Your main content here */}
+      </ContentWrapper>
+      <FooterContainer>
         <Left>
-            <Logo>usa shopping</Logo>
-            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit
-                lorem ipsum dolor sit amet consectetur adipisicing elit
-                lorem ipsum dolor sit amet consectetur adipisicing elit
-           </Desc>
-
-            <SocialContainer>
-                <SocialIcon color="3B5999">
-                    <Facebook />                    
-                </SocialIcon>
-
-                <SocialIcon color="3B5999">
-                    <Instagram />                    
-                </SocialIcon>
-
-                <SocialIcon color="3B5999">
-                    <Twitter />                    
-                </SocialIcon>
-               
-            </SocialContainer>
-
-
+          <Logo>usa shopping</Logo>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit lorem ipsum dolor sit amet consectetur
+            adipisicing elit lorem ipsum dolor sit amet consectetur adipisicing elit
+          </Desc>
+          <SocialContainer>
+            <SocialIcon color="3B5999">
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+              <Twitter />
+            </SocialIcon>
+          </SocialContainer>
         </Left>
-
         <Center>
-            <Title>Useful Links</Title>
-
-            <List>
-                <ListItem>Home</ListItem>
-                <ListItem>Cart</ListItem>
-                <ListItem>Man Fashion</ListItem>
-                <ListItem>Woman Fashion</ListItem>
-                <ListItem>Accessories</ListItem>
-                <ListItem>My Account</ListItem>
-                <ListItem>Order Tracking</ListItem>
-                <ListItem>Wishlist</ListItem>
-                <ListItem>Wishlist</ListItem>
-                <ListItem>Terms</ListItem>
-
-            </List>
-
-
-
-
-
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Woman Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
         </Center>
-
-
-
         <Right>
-            <Title>
-                Contact                
-            </Title>
-
-            <ContactItem>
-                <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
-            </ContactItem>
-
-
-            <ContactItem>   
-                <Phone style={{marginRight:"10px"}}/> +1 234 56 78
-            </ContactItem>
-
-
-            <ContactItem>
-                <MailOutline style={{marginRight:"10px"}}/> 0k7pH@example.com
-            </ContactItem>
-
-
-            <PaymentContainer>
-                <PaymentIconContainer>
-                    <PaymentIcon color="3B5999">
-                        <Facebook />
-                    </PaymentIcon>
-                </PaymentIconContainer>
-
-                <PaymentIconContainer>
-                    <PaymentIcon color="E4405F">
-                        <Instagram />
-                    </PaymentIcon>
-                </PaymentIconContainer>
-
-                <PaymentIconContainer>
-                    <PaymentIcon color="55ACEE">
-                        <Twitter />
-                    </PaymentIcon>
-
-
-                </PaymentIconContainer>
-
-                <PaymentIconContainer>
-                    <PaymentIcon color="E60023">
-                        <Pinterest />
-                    </PaymentIcon>
-
-
-                </PaymentIconContainer>
-
-                
-
-
-
-
-            </PaymentContainer>
-
-
-
-
+          <Title>Contact</Title>
+          <ContactItem>
+            <Room style={{ marginRight: '10px' }} /> 622 Dixie Path , South Tobinchester 98336
+          </ContactItem>
+          <ContactItem>
+            <Phone style={{ marginRight: '10px' }} /> +1 234 56 78
+          </ContactItem>
+          <ContactItem>
+            <MailOutline style={{ marginRight: '10px' }} /> 0k7pH@example.com
+          </ContactItem>
+          <PaymentContainer>
+            <PaymentIconContainer>
+              <PaymentIcon color="3B5999">
+                <Facebook />
+              </PaymentIcon>
+            </PaymentIconContainer>
+            <PaymentIconContainer>
+              <PaymentIcon color="E4405F">
+                <Instagram />
+              </PaymentIcon>
+            </PaymentIconContainer>
+            <PaymentIconContainer>
+              <PaymentIcon color="55ACEE">
+                <Twitter />
+              </PaymentIcon>
+            </PaymentIconContainer>
+            <PaymentIconContainer>
+              <PaymentIcon color="E60023">
+                <Pinterest />
+              </PaymentIcon>
+            </PaymentIconContainer>
+          </PaymentContainer>
         </Right>
+      </FooterContainer>
+    </PageWrapper>
+  );
+};
 
-
-
-
-
-
-    </Container>
-  )
-}
-
-export default Footer
+export default Footer;
--- shopping/src/pages/ProductList.jsx
+++ shopping/src/pages/ProductList.jsx
@@ -2,21 +2,53 @@ import React from 'react'
 import styled from 'styled-components'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
+import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
-const Container = styled.div`
-    
+
+const Container = styled.div`   
 
 `
 
+const Title = styled.h1`
+  margin: 20px;
+  
+`
 
+const FilterContainer = styled.div`
 
+  display: flex;
+  justify-content: space-between;
+
+`
+
+const Filter = styled.div`
+  margin: 20px;
+
+`
 
 const ProductList = () => {
   return (
     <Container>
         <Navbar />
         <Announcement />
+        <Title>Dresses</Title>
+          <FilterContainer>
+            <Filter>filter1</Filter>
+            <Filter>filter2</Filter>
 
+            <Products />
+            <Newsletter />
+            <Footer />
+
+
+
+
+
+
+
+          </FilterContainer>
     </Container>
   )
 }
