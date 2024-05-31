0549b3c4abd4d2bf4b6278dc7a2498f3fe258147
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-01 01:10:28 +0500

    z

---
+++ shopping/src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Container = 
+
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
--- shopping/src/pages/ProductList.jsx
+++ shopping/src/pages/ProductList.jsx
@@ -28,6 +28,25 @@ const Filter = styled.div`
   margin: 20px;
 
 `
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+
+
+
+
+`
+const Option = styled.option`
+    
+
+`
+
+
 
 const ProductList = () => {
   return (
@@ -36,8 +55,47 @@ const ProductList = () => {
         <Announcement />
         <Title>Dresses</Title>
           <FilterContainer>
-            <Filter>filter1</Filter>
-            <Filter>filter2</Filter>
+            <Filter>
+              <FilterText>Filter Products: </FilterText>
+              <Select>
+                <Option disabled selected>
+                  Color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Red</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+              </Select>            
+              
+              </Filter>
+            <Filter>
+              <FilterText>Sort Products: </FilterText>
+              <Select>
+                <Option disabled selected>
+                  Size
+                </Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+                <Option>XXL</Option>            
+              </Select>                                       
+              </Filter>
+
+              <Filter>
+              <FilterText>Sort Products: </FilterText>
+              <Select>
+                <Option selected Newest></Option>
+                <Option>Price (asc)</Option>
+                <Option>Price (desc)</Option>
+                          
+              </Select>                                       
+              </Filter>
+
+
 
             <Products />
             <Newsletter />
