c06f001776d638ebb5d0611c95920ce5cdff68f5
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-01 03:30:19 +0500

    330am 01 06 2024

--- shopping/src/pages/Product.jsx
+++ shopping/src/pages/Product.jsx
@@ -1,12 +1,67 @@
+// import { Announcement } from '@mui/icons-material'
 import React from 'react'
-import styled 
+import styled from 'styled-components'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import { Title } from '@mui/icons-material'
 
-const Container = styled-
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`
+
+const ImgContainer = styled.div`
+  flex: 1;
+`
+
+
+const Image = styled.img``
+
+
+const InfoContainer = styled.div``
+
+
+const Title = styled.h1``
+
+
+const Desc = styled.p``
+
+
+const Price = styled.span``
+
 
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <Container>
+        <Navbar />
+        <Announcement />
+          <Wrapper>
+            <ImgContainer>              
+              <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+            </ImgContainer>
+
+            <InfoContainer>
+                <Title>Denim Jumpsuit</Title>              
+                <Desc>Denim Jumpsuit </Desc>       
+                <Price>$ 20</Price>        
+            </InfoContainer>
+
+            
+
+
+
+
+          </Wrapper>
+        <Newsletter />
+        <Footer />
+
+    </Container>
   )
 }
 
