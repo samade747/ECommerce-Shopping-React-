4e7bb271caa5b88fd3745b1a63b9d1b683a7d163
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-02 10:46:26 +0500

    z

--- shopping/src/pages/Register.jsx
+++ shopping/src/pages/Register.jsx
@@ -1,8 +1,36 @@
+import { Title } from '@mui/icons-material'
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const Wrapper = styled.div``
+
+
+
+
+
+
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <Container>
+      <Wrapper>
+          <Title>CREATE AN ACCOUNT</Title>
+          <Form>
+              <Input placeholder="name"/>
+              <Input placeholder="last name"/>
+              <Input placeholder="username"/>
+              <Input placeholder="email"/>
+              <Input placeholder="password"/>
+              <Input placeholder="confirm password"/>
+              <Agreement>
+                  By creating an account, I consent to the processing of my personal
+                  data in accordance with the <b>PRIVACY POLICY</b>
+              </Agreement>
+              <Button>CREATE</Button>
+          </Form>
+      </Wrapper>
+    </Container>
   )
 }
 
