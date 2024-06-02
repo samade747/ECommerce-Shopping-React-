186a0428a2bd3b186dc764f769de735c05534b21
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-02 12:13:18 +0500

    1213pm 02 6 2024

--- shopping/src/pages/Login.jsx
+++ shopping/src/pages/Login.jsx
@@ -8,7 +8,16 @@ const Container = styled.div``
 const Login = () => {
   return (
     <Container>
-        
+      <Wrapper>
+          <Title>SIGN IN</Title>
+          <Form>
+              <Input placeholder="username"/>
+              <Input placeholder="password"/>            
+              <Button>LOGIN</Button>
+              <Link>FORGOT PASSWORD?</Link>
+              <Link>CREATE AN ACCOUNT</Link>
+          </Form>
+      </Wrapper>
     </Container>
   )
 }
--- shopping/src/pages/Register.jsx
+++ shopping/src/pages/Register.jsx
@@ -1,10 +1,70 @@
-import { Title } from '@mui/icons-material'
+// import { Title } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
-const Wrapper = styled.div``
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;  
+`
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
 
+`
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  
+
+`
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+`
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+  
+
+
+`
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  
+`
+
+const Agreement = styled.div`
+  font-size: 12px;
+  margin: 20px 0px;
+  
+` 
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`
 
 
 
@@ -24,8 +84,10 @@ const Register = () => {
               <Input placeholder="password"/>
               <Input placeholder="confirm password"/>
               <Agreement>
+                <span>
                   By creating an account, I consent to the processing of my personal
                   data in accordance with the <b>PRIVACY POLICY</b>
+                  </span>
               </Agreement>
               <Button>CREATE</Button>
           </Form>
