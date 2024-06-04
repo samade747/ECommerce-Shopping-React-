a8db5470d8e0c2ff686c33bbbbd4ffca1c45956e
samade747 <samad.e747@gmail.com>
2024-06-04 14:06:00 +0500

    06am 4 6 2024

--- shopping/src/pages/Login.jsx
+++ shopping/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
   width: 100vw;
@@ -15,6 +16,8 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;  
 
+  ${mobile({ flexDirection: 'column' })}
+
 ` 
 
 const Button = styled.button`
@@ -26,16 +29,24 @@ const Button = styled.button`
   cursor: pointer;
   margin-bottom: 10px;  
 
+  ${mobile({ width: '80%' })}
+
 `
 const Wrapper = styled.div`
   width: 25%;
   padding: 20px;
   background-color: white;
 
+  ${Button} {
+    width: 40%;
+  }
+
 ` 
 const Title = styled.h1`  
   font-size: 24px;
   font-weight: 300;
+
+  ${mobile({ fontSize: '20px' })}
 
 `
 
@@ -44,18 +55,25 @@ const Form = styled.form`
   display: flex;  
   flex-direction: column;
 
+  ${mobile({ width: '80%' })}
+
 `
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
   margin: 10px 0px;
   padding: 10px;
+
+  ${mobile({ width: '80%' })}
+
 `
 const Link = styled.a`
   margin: 5px 0px;
   font-size: 12px;
   text-decoration: underline;
   cursor: pointer;
+
+  ${mobile({ fontSize: '10px' })}
 `
 
 const Login = () => {
